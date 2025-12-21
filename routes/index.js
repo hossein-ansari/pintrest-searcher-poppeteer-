e428cbfdd30d5f87b7e13bd183d2113096b53f4b
Hossein-ansari <hossein838485@gmail.com>
2025-12-20 23:21:39 -0800

    make seprate the modules

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ const scrapeController = require('../controllers/scrapeController');
 
 router.get('/search', scrapeController.searchImmediate);
 router.post('/download', scrapeController.downloadImmediate);
-router.get('/convertAndDownload', scrapeController.downloadAndConverterImageImmediate);
 
 module.exports = router;
--- services/jobService.js
+++ services/jobService.js
@@ -1,6 +1,5 @@
 const { scrapePinterest } = require('./scraperService');
 const { downloadImage } = require('./downloadService');
-const { downloadAndConverterImage } = require('./convertandDownloadimage');
 
 async function scrapeNow(query, count) {
   return await scrapePinterest(query, count);
@@ -8,11 +7,8 @@ async function scrapeNow(query, count) {
 async function downloadNow(res, url) {
   return await downloadImage(res, url);
 }
-async function downloadAndConverterImageNow(req,res) {
-  return await downloadAndConverterImage(req,res);
-}
 async function downloadNow(res, url) {
   return await downloadImage(res, url);
 }
 
-module.exports = { scrapeNow,downloadNow ,downloadAndConverterImageNow};
+module.exports = { scrapeNow,downloadNow };
